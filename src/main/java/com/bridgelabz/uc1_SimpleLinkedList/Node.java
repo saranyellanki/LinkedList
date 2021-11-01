82ab8be1c2b44a7081d6f4f2174db3c6133088a4
saranyellanki <saran.yellanki12@gmail.com>
2021-11-01 18:08:19 +0530

    Added simple linked list

---
+++ src/main/java/com/bridgelabz/uc1_SimpleLinkedList/Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.uc1_SimpleLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
