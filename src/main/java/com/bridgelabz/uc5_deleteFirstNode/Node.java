23f5053c8364666d36858337f113f51045ddbbbf
saranyellanki <saran.yellanki12@gmail.com>
2021-11-01 23:34:03 +0530

    Added pop method

---
+++ src/main/java/com/bridgelabz/uc5_deleteFirstNode/Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.uc5_deleteFirstNode;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
